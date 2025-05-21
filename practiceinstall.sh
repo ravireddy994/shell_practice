515e07b67144cd10ceed3b2cd8ffcde15aea7baa
Dell@DESKTOP-SQ81EOD <Dell@DESKTOP-SQ81EOD>
2025-05-21 14:35:10 +0530

    file1

--- practiceinstall.sh
+++ practiceinstall.sh
@@ -2,26 +2,25 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-echo "Error: Run with sudo/root user"
-exit 1
+    echo "Error: Run with sudo/root user"
+    exit 1
 else
-echo "Running with root User"
+    echo "Running with root User"
 fi
 
 dnf list installed mysql
 
 if [ $? -eq 0 ]
 then
-echo "mysql Already installed : good to go"
-exit 1
+    echo "mysql Already installed : good to go"
+    exit 1
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "install failed"
+    else
+        echo "installed sucess"
+    fi
 else
-echo "my sql is not installed : goinig to install"
-fi
-
-dnf install mysql -y
-if [ $? -ne 0 ]
-then
-echo "install failed"
-else
-echo "installed sucess"
+    echo "my sql is not installed : goinig to install"
 fi
