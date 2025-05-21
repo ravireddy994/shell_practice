a1ffac7f2f316fe938ca789bf85cec877baef824
Dell@DESKTOP-SQ81EOD <Dell@DESKTOP-SQ81EOD>
2025-05-21 13:01:06 +0530

    file1

--- installcommand.sh
+++ installcommand.sh
@@ -8,3 +8,13 @@ exit 1
 else
 echo "you are running with root user"
 fi
+
+dnf install mysql -y
+
+if [ $? -eq 0 ]
+then
+echo "mysql installed SUCESS"
+else
+echo "mysql installed failed"
+exit 1
+fi
