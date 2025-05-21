8093bfc2c98eab71a15f84a9f6659c90ddb95a8f
Dell@DESKTOP-SQ81EOD <Dell@DESKTOP-SQ81EOD>
2025-05-21 13:10:49 +0530

    file1

--- installcommand.sh
+++ installcommand.sh
@@ -3,28 +3,27 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-echo "error: run with sudo user"
-exit 1
+    echo "error: run with sudo user"
+    exit 1
 else
-echo "you are running with root user"
-fi
-
-dnf install mysql -y
-
-if [ $? -eq 0 ]
-then
-echo "mysql installed SUCESS"
-else
-echo "mysql installed failed"
-exit 1
+    echo "you are running with root user"
 fi
 
 dnf list installed mysql
 
 if [ $? -ne 0 ]
 then
-echo "mysql not installed : going to install"
+    echo "mysql not installed : going to install"
+    dnf install mysql -y
+
+    if [ $? -eq 0 ]
+    then
+        echo "mysql installed SUCESS"
+    else
+        echo "mysql installed failed"
+        exit 1
+    fi
 else
-echo "mysql already installed : good to go"
-exit 1
+    echo "mysql already installed : good to go"
+    exit 1
 fi
